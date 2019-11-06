fb701571cf2e9c166fdac11882712e6b65269969
Fuzzy <rodrigo.rosa.005@acad.pucrs.br>
2019-11-06 11:28:08 -0300

    Update fib.hs

--- examples/fib.hs
+++ examples/fib.hs
@@ -1,3 +1,9 @@
 fib 0 = 0
 fib 1 = 1
 fib n = if n > 1 then fib(n-1) + fib(n-2) else 0
+
+-- Guard
+fib n | (n == 0) = 0
+      | (n == 1) = 1
+      | (n > 1) = fib(n-1) + fib(n-2)
+      | otherwise  = 0
