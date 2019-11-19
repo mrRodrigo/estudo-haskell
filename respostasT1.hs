c100d30b4b0b9cfe0338f63d42bd7efe73841d80
Fuzzy <rodrigo.rosa.005@acad.pucrs.br>
2019-11-19 09:22:37 -0300

    Update respostasT1.hs

--- respostasT1.hs
+++ respostasT1.hs
@@ -35,3 +35,13 @@ myMap :: (a -> b) -> (a -> b) -> [a] -> [b]
 myMap f1 f2 (x:y:xs) = ([f1 x, f2 y]++(myMap f1 f2 xs))
 myMap _ _ [] = []
 myMap f1 _ (x:[]) = [f1 x]
+
+
+--  implemente o algoritmo de Luhn para a validações de números de cartão de crédito para códigos de cartão de qualquer tamanho.
+
+nineRole :: Int -> Int
+nineRole a = if (a * 2) < 9 then a * 2 else (a * 2) - 9
+
+luhn :: [Int] -> Bool
+luhn [] = False
+luhn xs = if ((sum (myMap (*1) (nineRole) xs)) `mod` 10) == 0 then True else False
